a4e8e39bf33bf270e8c779f194460e9b927c0d7c
Loop1397 <shwlsdn0906@gmail.com>
2024-05-24 23:37:54 +0900

    🎨Feat: Cors 권한 설정
    서버로오는 각종 리퀘스트에 대한 response 권한을 설정하기 위해 Cors 작성(현재 모든 요청에 응답 가능)

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+// cors 권한 관련
+app.use(cors());
 // /api로 요청이 올 시 taskApi를 호출
 app.use('/api', indexRouter);
 
